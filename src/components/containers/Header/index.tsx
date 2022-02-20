d245dac4d81bb1b044fae8843f7bdab04290bc32
pksung1 <pksung1@gmail.com>
2022-02-20 23:19:20 +0900

    feat(Header): added Layout

--- src/components/containers/Header/index.tsx
+++ src/components/containers/Header/index.tsx
@@ -1,6 +1,31 @@
+import { useTheme } from '@emotion/react';
+import { css, jsx } from '@emotion/react'
+
+
+
 const Header = () => {
+  const theme = useTheme();
   return (
-    <div></div>
+    <header css={css`
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 1rem 1.5rem;
+      box-sizing: border-box;
+    `}>
+      <nav css={css`
+        width: 5rem;
+        color: ${theme.color.text}
+      `}>Menu</nav>
+      <div css={css`
+        color: ${theme.color.text}
+      `}>Wordle</div>
+      <div css={css`
+        width: 5rem;
+        color: ${theme.color.text}
+      `}>Setting</div>
+    </header>
   )
 }
 export default Header;
