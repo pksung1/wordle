95c2f49845a27ddce5991e81614bb9f3146c3d52
pksung1 <pksung1@gmail.com>
2022-02-20 23:18:36 +0900

    feat(theme): Added Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { GameBoard, Header, Keyboard } from '@/components/containers'
+import { ThemeProvider, css } from '@emotion/react'
+
+
+const theme = {
+  color: {
+    bg: '#000000',
+    text: '#FFFFFF',
+  },
+}
+
 
 function App() {
   return (
-    <div></div>
+    <ThemeProvider theme={theme}>
+      <main css={css`
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        background: ${theme.color.bg};
+        color: ${theme.color.text};
+      `}>
+        
+        <Header />
+        <section css={css`
+          width: 100%;
+          max-width: 30rem;
+          display: flex;
+          flex-direction: column;
+          flex: 1;
+          margin: 0 auto;
+        `}>
+          <GameBoard />
+          <Keyboard />
+        </section>
+      </main>
+    </ThemeProvider>
   )
 }
 
