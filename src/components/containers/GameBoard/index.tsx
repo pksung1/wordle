378ee1db610030456a6ded76a5af634aa687a000
pksung1 <pksung1@gmail.com>
2022-02-20 23:19:09 +0900

    feat(Gameboard): added Layout

--- src/components/containers/GameBoard/index.tsx
+++ src/components/containers/GameBoard/index.tsx
@@ -1,6 +1,13 @@
+import { css } from "@emotion/react";
+
 const GameBoard = () => {
   return (
-    <div></div>
+    <div css={css`
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}>GameBoard</div>
   )
 }
 
