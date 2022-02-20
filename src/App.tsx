a029f7f99dbc331e3f97b0284e18e982ad22a948
pksung1 <pksung1@gmail.com>
2022-02-21 03:07:05 +0900

    feat(recoil): added recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { GameBoard, Header, Keyboard } from '@/components/containers'
 import { ThemeProvider, css } from '@emotion/react'
+import { RecoilRoot } from 'recoil'
 
 
 const theme = {
@@ -13,28 +14,30 @@ const theme = {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <main css={css`
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        background: ${theme.color.bg};
-        color: ${theme.color.text};
-      `}>
-        
-        <Header />
-        <section css={css`
-          width: 100%;
-          max-width: 30rem;
+      <RecoilRoot>
+        <main css={css`
+          width: 100vw;
+          height: 100vh;
           display: flex;
           flex-direction: column;
-          flex: 1;
-          margin: 0 auto;
+          background: ${theme.color.bg};
+          color: ${theme.color.text};
         `}>
-          <GameBoard />
-          <Keyboard />
-        </section>
-      </main>
+          
+          <Header />
+          <section css={css`
+            width: 100%;
+            max-width: 30rem;
+            display: flex;
+            flex-direction: column;
+            flex: 1;
+            margin: 0 auto;
+          `}>
+            <GameBoard />
+            <Keyboard />
+          </section>
+        </main>
+      </RecoilRoot>
     </ThemeProvider>
   )
 }
---
+++ src/store/atoms.ts
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const wordleState = atom({
+  key: 'wordleState',
+  default: ['', '', '', '', '', '']
+})
+
+export const currentWordleIndexState = atom({
+  key: 'currentWordleIndexState',
+  default: 0,
+})
