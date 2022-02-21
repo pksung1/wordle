6d4b09baecc39989274e907396bf50ff10a1e4fa
pksung1 <pksung1@gmail.com>
2022-02-21 13:37:32 +0900

    feat(recoil): think gameboard logic

--- src/components/containers/GameBoard/index.tsx
+++ src/components/containers/GameBoard/index.tsx
@@ -1,13 +1,62 @@
-import { css } from "@emotion/react";
+import { wordleState } from "@/store/atoms";
+import { useUpdateWordle } from "@/store/hooks";
+import { css, useTheme } from "@emotion/react";
+import { createRef, memo, useEffect, useRef, useState } from "react";
+import { useRecoilValue } from "recoil";
+
+
+type WordRowProps = {
+  word: string;
+}
+const _WordRow: React.FC<WordRowProps> = ({word}) => {
+
+  const theme = useTheme();
+  return (
+    <>
+      {word.padEnd(5).split('').map((w) => (<div css={css`
+          aspect-ratio: 1;
+          box-sizing: border-box;
+          border: 0.1rem solid ${theme.color.text};
+          border-radius: 0.5rem;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 3rem
+        `}>
+          {w}
+        </div>
+      ))}
+    </>
+  )
+}
+
+// 메모라이징으로 불필요한 렌더링을 줄임
+const WordRow = memo(_WordRow, (prevProp, nextProp) => {
+  return nextProp.word === prevProp.word
+})
+
 
 const GameBoard = () => {
+  const theme = useTheme();
+  useUpdateWordle();
+  const wordles = useRecoilValue(wordleState)
   return (
     <div css={css`
       flex: 1;
       display: flex;
-      justify-content: center;
       align-items: center;
-    `}>GameBoard</div>
+      padding: 1rem;
+    `}>
+      <div css={css`
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+        grid-gap: 1rem;
+      `}>
+        {wordles.map((word, idx) => <WordRow word={word} key={word + idx}/>)}
+      </div>
+    </div>
   )
 }
 
---
+++ src/store/hooks.ts
@@ -0,0 +1,42 @@
+import { useEffect } from "react"
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
+import { currentWordleIndexState, wordleState } from "./atoms"
+import { getCurrentWordle } from "./select"
+
+/**
+ * Wordle 키입력 이벤트를 처리하는 부분
+ */
+const useUpdateWordle = () => {
+  const [wordles, setWordles] = useRecoilState(wordleState)
+  const [currentWordleIndex, setCurrentWordleIndex] = useRecoilState(currentWordleIndexState)
+  const currentWordle = useRecoilValue(getCurrentWordle)
+  
+  const handleKeyPress = (e: KeyboardEvent) => {
+    const key = e.key.toUpperCase()
+    let nextWordle = currentWordle
+    if (key.length === 1 && 'A' <= key && key <= 'Z') {
+      nextWordle = (currentWordle + key).slice(0, 5)
+    } else if (key === 'BACKSPACE') {
+      nextWordle = currentWordle.slice(0, currentWordle.length - 1)
+    } else if (key === 'ENTER' && currentWordleIndex < 5) {
+      setCurrentWordleIndex(currentWordleIndex + 1)
+    }
+
+    const nextWordles = [...wordles]
+    nextWordles[currentWordleIndex] = nextWordle;
+    setWordles(nextWordles)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress)
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress)
+    }
+  } ,[currentWordle, currentWordleIndex])
+
+  return {handleKeyPress}
+}
+
+export {
+  useUpdateWordle
+}
