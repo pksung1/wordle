0afbd8fd8723d4010533b71c4adc2457fbd0bb3c
seonpark <pksung1@gmail.com>
2022-02-26 11:32:05 +0900

    refactor(eslint) : 린트 수정

---
+++ src/containers/Header/index.tsx
@@ -0,0 +1,43 @@
+import { useTheme } from '@emotion/react'
+import { css } from '@emotion/react'
+
+const Header = () => {
+  const theme = useTheme()
+  return (
+    <header
+      css={css`
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1rem 1.5rem;
+        box-sizing: border-box;
+      `}
+    >
+      <nav
+        css={css`
+          width: 5rem;
+          color: ${theme.color.text};
+        `}
+      >
+        Menu
+      </nav>
+      <div
+        css={css`
+          color: ${theme.color.text};
+        `}
+      >
+        Wordle
+      </div>
+      <div
+        css={css`
+          width: 5rem;
+          color: ${theme.color.text};
+        `}
+      >
+        Setting
+      </div>
+    </header>
+  )
+}
+export default Header
